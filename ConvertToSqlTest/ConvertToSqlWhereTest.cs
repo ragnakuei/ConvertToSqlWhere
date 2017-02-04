eeac15067baec1ff0da8b8a189a06d29da83eb86
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-04 18:49:12 +0800

    initial commit

---
+++ ConvertToSqlTest/ConvertToSqlWhereTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConvertToSqlTest
+{
+    [TestClass]
+    public class ConvertToSqlWhereTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
