069a2dc78ee110327a3e2631f32c68aed46465da
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-04 19:48:26 +0800

    1st green light

--- ConvertToSql/ConvertToSql.cs
+++ ConvertToSql/ConvertToSql.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConvertToSqlLibrary
 {
     public class ConvertToSql
     {
-        public object ToWhere(string input)
+        public string ToWhere(string input)
         {
-            throw new NotImplementedException();
+            string regexPattern = @"(.*?):equals\((\d*?)\)";
+            var result = "where ";
+
+            var match = Regex.Match(input, regexPattern);
+            if (match.Success)
+            {
+                var regexReplace = "$1 = $2";
+                result += Regex.Replace(match.Value, regexPattern, regexReplace);
+            }
+            return result;
         }
     }
 }
