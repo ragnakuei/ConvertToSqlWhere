6b001dfd0e7683b97cb3790f613277248708953e
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-04 20:24:46 +0800

    5th red light

--- ConvertToSqlTest/ConvertToSqlWhereTest.cs
+++ ConvertToSqlTest/ConvertToSqlWhereTest.cs
@@ -54,5 +54,18 @@ namespace ConvertToSqlTest
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ToWhere_not_equals_string()
+        {
+            var target = new ConvertToSql();
+            var input = "not(age:equals(\"20\"))";
+            var expected = "where age <> '20'";
+
+            var actual = target.ToWhere(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
     }
 }
